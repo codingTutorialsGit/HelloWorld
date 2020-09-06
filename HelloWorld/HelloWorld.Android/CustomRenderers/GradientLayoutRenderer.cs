583784265aba4860d223c78f2d48e8d8f1490ca4
codingTutorialsGit <tubetutorialsawesome@gmail.com>
2020-09-06 17:41:27 +1200

    Add project files.

---
+++ HelloWorld/HelloWorld.Android/CustomRenderers/GradientLayoutRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using HelloWorld.CustomControls;
+using HelloWorld.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.FastRenderers;
+
+[assembly: ExportRenderer(typeof(GradientLayout), typeof(GradientLayoutRenderer))]
+namespace HelloWorld.Droid.CustomRenderers
+{
+    public class GradientLayoutRenderer : VisualElementRenderer<StackLayout>
+    {
+        private Color StartColor { get; set; }
+        private Color EndColor { get; set; }
+
+        public GradientLayoutRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void DispatchDraw(Android.Graphics.Canvas canvas)
+        {
+
+            var gradient = new Android.Graphics.LinearGradient(0, 0, Width, Height, this.StartColor.ToAndroid(), this.EndColor.ToAndroid(), Android.Graphics.Shader.TileMode.Mirror);
+
+            var paint = new Android.Graphics.Paint()
+            {
+                Dither = true
+            };
+
+            paint.SetShader(gradient);
+            canvas.DrawPaint(paint);
+
+            base.DispatchDraw(canvas);
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<StackLayout> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || e.NewElement == null) return;
+
+            try
+            {
+                var layout = e.NewElement as GradientLayout;
+                this.StartColor = layout.StartColor;
+                this.EndColor = layout.EndColor;
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/CustomControls/GradientLayout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HelloWorld.CustomControls
+{
+    public class GradientLayout : StackLayout
+    {
+        public Color StartColor { get; set; }
+        public Color EndColor { get; set; }
+    }
+}
