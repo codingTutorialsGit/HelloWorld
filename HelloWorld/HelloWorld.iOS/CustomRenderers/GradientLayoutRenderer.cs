aa47833417e2aa792b04fa2e02b0a5d200ed01cb
codingTutorialsGit <tubetutorialsawesome@gmail.com>
2020-09-06 18:39:41 +1200

    added custom gradient layout renderer for iOS

---
+++ HelloWorld/HelloWorld.iOS/CustomRenderers/GradientLayoutRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using HelloWorld.CustomControls;
+using HelloWorld.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(GradientLayout), typeof(GradientLayoutRenderer))]
+namespace HelloWorld.iOS.CustomRenderers
+{
+    public class GradientLayoutRenderer : VisualElementRenderer<StackLayout>
+    {
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+            GradientLayout layout = (GradientLayout)this.Element;
+            CGColor startColor = layout.StartColor.ToCGColor();
+            CGColor endColor = layout.EndColor.ToCGColor();
+
+            var gradient = new CAGradientLayer()
+            {
+                StartPoint = new CGPoint(0, 0.5),
+                EndPoint = new CGPoint(1, 0.5)
+            };
+
+            gradient.Frame = rect;
+            gradient.Colors = new CGColor[] { startColor, endColor };
+            NativeView.Layer.InsertSublayer(gradient, 0);
+        }
+
+    }
+}
